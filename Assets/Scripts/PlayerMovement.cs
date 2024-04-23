2dc7c08a7f1ebc50528e5d03798621706ce47b96
DanCasber <daniela.castillo@uniat.edu.mx>
2024-04-23 00:49:56 -0600

    Players placement, placement screen, exit, restart, walls colliders, audio

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,28 +1,47 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using TMPro;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviourPunCallbacks
 {
 
     [SerializeField] private float speed;
     [SerializeField] private PhotonView view;
     [SerializeField] private TextMeshProUGUI lapText;
+    [SerializeField] private GameObject placementCanvas;
+    [SerializeField] private TextMeshProUGUI placementText;
+    [SerializeField] private TextMeshProUGUI placementShadowText;
 
     private int lapsCompleted = 0;
     private int totalLaps = 3;
-    public static int placement = 0;
 
     void Start()
     {
+        view = GetComponent<PhotonView>();
         if (view.IsMine)
         {
+            placementCanvas = GameObject.FindGameObjectWithTag("PlacementCanvas");
             lapText = GameObject.FindGameObjectWithTag("NumLaps").GetComponent<TextMeshProUGUI>();
             UpdateLapText();
         }
+
+        // Reset placement to 0 when the scene starts
+        if (PhotonNetwork.IsMasterClient)
+        {
+            ResetPlacement();
+        }
+    }
+
+    private void ResetPlacement()
+    {
+        // Reset placement in room properties
+        ExitGames.Client.Photon.Hashtable customProps = new ExitGames.Client.Photon.Hashtable();
+        customProps["Placement"] = 0;
+        PhotonNetwork.CurrentRoom.SetCustomProperties(customProps);
     }
 
     void Update()
@@ -42,12 +61,15 @@ public class PlayerMovement : MonoBehaviour
         {
             lapsCompleted++;
             UpdateLapText();
-            Debug.Log("Goal Reached");
             if (lapsCompleted > totalLaps)
             {
-                placement++;
-                Debug.Log("Race Finished In " + placement + " Place.");
-                PhotonNetwork.LoadLevel("RaceFinished");
+                // Increment placement and get the updated value
+                int placement = IncrementPlacement(); 
+                SetChildrenActive(placementCanvas, true);
+                placementText = GameObject.FindGameObjectWithTag("Placement").GetComponent<TextMeshProUGUI>();
+                placementShadowText = GameObject.FindGameObjectWithTag("PlacementShadow").GetComponent<TextMeshProUGUI>();
+                placementText.text = GetPlacementText(placement);
+                placementShadowText.text = GetPlacementText(placement);
             }
         }
     }
@@ -58,6 +80,40 @@ public class PlayerMovement : MonoBehaviour
         {
             // Display lap counts for all players
             lapText.text = lapsCompleted + "/" + totalLaps;
+        }
+    }
+
+    private int IncrementPlacement()
+    {
+        // Increment placement for all players
+        int currentPlacement = PhotonNetwork.CurrentRoom.CustomProperties["Placement"] != null ? (int)PhotonNetwork.CurrentRoom.CustomProperties["Placement"] : 0;
+        ExitGames.Client.Photon.Hashtable customProps = new ExitGames.Client.Photon.Hashtable();
+        customProps["Placement"] = currentPlacement + 1;
+        PhotonNetwork.CurrentRoom.SetCustomProperties(customProps);
+
+        return currentPlacement + 1;
+    }
+
+    private string GetPlacementText(int placement)
+    {
+        switch (placement)
+        {
+            case 1:
+                return "1st";
+            case 2:
+                return "2nd";
+            case 3:
+                return "3rd";
+            default:
+                return placement + "th";
+        }
+    }
+
+    private void SetChildrenActive(GameObject parent, bool active)
+    {
+        foreach (Transform child in parent.transform)
+        {
+            child.gameObject.SetActive(active);
         }
     }
 }
